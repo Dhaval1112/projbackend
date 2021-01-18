2ffdc5331f5313b1921f61f039222a863386b803
JDBrothers1016 <jp1016charusat@gmail.com>
2021-01-18 10:57:07 +0530

    with virtual and function in schema << mongoose >> user.js

---
+++ models/user.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+import crypto from "crypto";
+import { v4 as uuidv4 } from "uuid";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+  encry_password: {
+    type: String,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv4();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+// here we created function which will encrypt user password and store those data in encry_password field
+userSchema.method = {
+  authanticate: function (plainpassword) {
+    return securePassword(plainpassword) === this.encry_password;
+  },
+
+  securePassword: function (plainpassword) {
+    if (!plainpassword) return "";
+
+    try {
+      // this is the code which will encrypt our password if available
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainpassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+// this is for compilation of model << mongoose.model() >>
+module.exports = mongoose.model("User", userSchema);
