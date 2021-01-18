a0c75b5baa11ebc3694335e00f03c2a6ebd08c37
JDBrothers1016 <jp1016charusat@gmail.com>
2021-01-18 22:52:29 +0530

    order and product Schema added

---
+++ models/product.js
@@ -0,0 +1,47 @@
+import { times } from "lodash";
+import mongoose from "mongoose";
+
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 250,
+    },
+    price: {
+      type: Number,
+      required: true,
+      maxlength: 12,
+      trim: true,
+    },
+    //   this field is type of category Schema
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stock: {
+      type: Number,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
